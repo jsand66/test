a9e86ba725947d0ff2830fddf89d12b60722b237
jsand66 <32664212+jsand66@users.noreply.github.com>
2018-10-09 11:02:26 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+package com.amazonaws.samples;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Hello implements RequestHandler<Object, String> {
+
+    @Override
+    public String handleRequest(Object input, Context context) {
+        return "Hello from Lambda";
+    }
+
+}
